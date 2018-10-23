67a3bb225e03ba80bbb8544d7d88fd13f540303b
Ariella Wills <Ariellaw@gmail.com>
2018-10-23 12:48:58 +0300

    added pictures and render picture function

--- js/imgs-service.js
+++ js/imgs-service.js
@@ -1,10 +1,12 @@
 'use strict'
 const IMGS_KEY = 'imgs';
 
+
+
 // var img = createImg('img/popo.jpg',['happy']);
 // console.log(img);
 
-function createImg(url,keywords) {
+function  {
     return {
         id: makeId(),
         url,
@@ -12,6 +14,23 @@ function createImg(url,keywords) {
     }
 }
 
+
 function createImgs(){
     if(!localStorage.)    
+else{
+    gImgs =
+     [ 
+        createImg('meme-imgs/2.jpg',['happy','dancing', 'carefree']),
+        createImg('meme-imgs/003.jpg', ['trump', 'angry', 'stupid','powerful']),
+        createImg('meme-imgs/004.jpg',['puppy', 'love', 'cute', 'friendship']),
+        createImg('meme-imgs/005.jpg',['sleeping', 'baby', 'puppy','calm', 'friendship']),
+        createImg('meme-imgs/5.jpg',['baby', 'payback']),
+        createImg('meme-imgs/006.jpg',['cat', 'sleeping','computer']),
+        createImg('meme-imgs/8.jpg',['Willy Wanka', 'sarcasm']),
+        createImg('meme-imgs/9.jpg',['baby', 'sneeky']),
+    ];
+}
+
+function getMemes(){
+    return  gImgs;
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-'use srtict'
+'use strict'
 
 console.log('main');
+
+function renderImgs(){
+    var elMemeContainer = document.querySelector('.meme-container');
+    var memeImgs =  getMemes();
+   var memeDisplay = memeImgs.map(img =>{
+       return `<img src=${img.url}>`
+    })
+    elMemeContainer.innerHTML = memeDisplay;
+}
 
 var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
 var gMeme = {
