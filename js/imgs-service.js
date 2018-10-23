49b3edd75c66842cf3144bfbb89755c1f8234b88
zeevgg <zeev.gerstner@gmail.com>
2018-10-23 11:38:45 +0300

    pusg strcture

---
+++ js/imgs-service.js
@@ -0,0 +1,17 @@
+'use strict'
+const IMGS_KEY = 'imgs';
+
+// var img = createImg('img/popo.jpg',['happy']);
+// console.log(img);
+
+function createImg(url,keywords) {
+    return {
+        id: makeId(),
+        url,
+        keywords,
+    }
+}
+
+function createImgs(){
+    if(!localStorage.)    
+}
